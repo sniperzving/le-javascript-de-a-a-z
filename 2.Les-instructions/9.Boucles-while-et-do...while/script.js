31a8292ccdab26aa7b1aae20ab7ff85f8e4c7c38
Ghiles Crp <ghiles.corp@gmail.com>
2023-04-30 19:56:25 +0200

    Ajout de 2.Instructions- Boucles while et do while

--- 2.Les-instructions/9.Boucles-while-et-do...while/script.js
+++ 2.Les-instructions/9.Boucles-while-et-do...while/script.js
@@ -6,7 +6,11 @@
     Attention, si vous avez l'auto-save d'activé, cela peut provoquer une boucle infinie qui fait bugger votre navigateur.
 */
 
-
+let engineTemperature = 20;
+while (engineTemperature < 100) {
+  console.log(engineTemperature);
+  engineTemperature += 10;
+}
 
 /* 
     2. La boucle do...while
@@ -14,3 +18,10 @@
     Boucle peu utilisée qui ressemble au while ou à la for loop, à l'exception près qu'elle exécute une instruction au moins une fois.
 */
 
+let num = 1;
+do {
+  num++;
+  console.log();
+} while (num < 10);
+
+console.log(num);
