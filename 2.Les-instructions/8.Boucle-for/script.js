a377e9bd4030734d5ea89c402223c17b050ac64f
Ghiles Crp <ghiles.corp@gmail.com>
2023-04-30 19:44:33 +0200

    Ajout de 2.Instructions- Boucle For

--- 2.Les-instructions/8.Boucle-for/script.js
+++ 2.Les-instructions/8.Boucle-for/script.js
@@ -17,24 +17,21 @@
 
 // boucle avec incrémentation
 
-
-
+for (let i = 0; i < 10; i++) console.log(i);
 
 // Nous pouvons aussi décrémenter
 
-
-
+for (let i = 9; i >= 0; i--) console.log(i);
 
 // Ou effectuer toutes les opérations possibles sur l'expression initiale
 
-
-
+for (let i = 0; i < 100; i += 10) console.log(i);
 
 // On utilise souvent une boucle for pour effectuer une opération sur tous les éléments d'un tableau.
-const numbers = [1,2,3,4,5];
+const numbers = [1, 2, 3, 4, 5];
 
-
-
+for (let i = 0; i < numbers.length; i++) console.log(numbers[i]++);
+console.log(numbers);
 
 /* 
     2. Il existe deux instructions utiles dans les boucles : break et continue.
@@ -44,4 +41,14 @@ const numbers = [1,2,3,4,5];
     L'instruction continue permet de passer à l'itération suivante plus tôt.
 */
 
+// BREAK
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+  if (i === 5) break;
+}
 
+// CONTINUE
+for (let i = 1; i <= 10; i++) {
+  if (i === 5) continue;
+  console.log(i);
+}
