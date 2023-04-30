a0d823ab5eef0faa95dae15c6325c96320071d84
Ghiles Crp <ghiles.corp@gmail.com>
2023-04-30 19:12:52 +0200

    Ajout de 2.Instructions- Comparaisons complexes

--- 2.Les-instructions/4.Comparaisons-complexes/script.js
+++ 2.Les-instructions/4.Comparaisons-complexes/script.js
@@ -6,14 +6,33 @@ const user1 = {
   name: "Victor",
   age: 35,
   admin: true,
-  position: "CEO"
-}
+  position: "CEO",
+};
 const user2 = {
   name: "Paul",
   age: 25,
   admin: false,
-  position: "Employee"
+  position: "Employee",
+};
+
+// A EVITER
+/* function checkPermission(user) {
+  if (
+    (user.age >= 30 && user.admin && user.position === "CEO") ||
+    user.position === "Director"
+  ) {
+    return "Accès autorisé"
+  } else {
+    return "accès refusé"
+  }
+} */
+
+// MIEUX
+function checkPermission(user) {
+  if (!user.admin || user.age < 30) return "Accès refusé";
+
+  if (user.position && user.age >= 30) return "Accès autorisé";
+  
 }
 
-
-
+console.log(checkPermission(user1));
