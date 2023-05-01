301e89b64a6903806828cac8b99f13467ee1252b
Ghiles Crp <ghiles.corp@gmail.com>
2023-05-01 20:47:58 +0200

    Modif de 3. Fonctions dans le détail 4. Les fonctions pures

--- 3.Les-fonctions-dans-le-détail/4.Les-fonctions-pures/script.js
+++ 3.Les-fonctions-dans-le-détail/4.Les-fonctions-pures/script.js
@@ -7,3 +7,47 @@
     B. Elle n'utilise aucune valeur en dehors de sa portée et n'a pas d'effets secondaires en dehors de son bloc.
 
 */
+
+// fonction pure
+function addPure(a, b) {
+  return a + b;
+}
+
+console.log(addPure(2, 2));
+
+let total = 0;
+
+//  fonction non pure car elle modifie total qui est en dehors de  son champ lexical
+function addNonPure(a, b) {
+  total = a + b;
+  return total;
+}
+
+console.log(addNonPure(2, 2));
+
+
+
+// addPure
+function getFinalPricePure(rawPrice, VAT) {
+    
+    return rawPrice * (100 + VAT) / 100;
+}
+
+console.log(getFinalPricePure(110, 20));
+// impure 
+
+let VAT = 20;
+function getFinalPriceNonPure(rawPrice) {
+    
+    return rawPrice * (100 + VAT) / 100;
+}
+
+console.log(getFinalPriceNonPure(110));
+
+
+// Impure
+function getViewportWidth() {
+    return window.innerWidth;
+}
+
+console.log(getViewportWidth());
