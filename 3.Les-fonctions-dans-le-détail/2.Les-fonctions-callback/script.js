cbb95c028feca80f43be460f368f5d5b0ff25c4a
Ghiles Crp <ghiles.corp@gmail.com>
2023-05-01 14:18:23 +0200

    Modif de 3. Fonctions dans le détail 2. Les fonctions call back

--- 3.Les-fonctions-dans-le-détail/2.Les-fonctions-callback/script.js
+++ 3.Les-fonctions-dans-le-détail/2.Les-fonctions-callback/script.js
@@ -23,6 +23,7 @@ const database = [
 // Coder une fonction qui vérifie l'âge d'un ensemble d'objets dans un tableau.
 // Elle retournera false au premier membre qui a moins de 18 ans.
 
+// En utilisant la boucle for et fonction fléchée
 function areLegal(arr, callback){
 
   for(let i = 0; i < arr.length; i++) {
@@ -34,5 +35,26 @@ function areLegal(arr, callback){
   return true;
 }
 
-// on peut tester toute sorte d'age.
 console.log(areLegal(database, age => age >= 18));
+
+// En utilisant la boucle for...of et fonction normale
+function isLegal(arr, callback) {
+
+  for(let person of arr) {
+
+    if(!callback(person.age)){
+      return false;
+    }
+
+  }
+
+  return true;
+}
+
+function checkAge(age) {
+  return age >= 18;
+}
+
+// on peut tester toute sorte d'age.
+
+console.log(isLegal(database, checkAge));
