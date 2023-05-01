32ff34ceac6deda50aa7e14ca268ad52f93ab685
Ghiles Crp <ghiles.corp@gmail.com>
2023-05-01 21:15:57 +0200

    Modif de 3. Fonctions dans le détail 6.Fonctions-auto-invoquées

--- 3.Les-fonctions-dans-le-détail/6.Fonctions-auto-invoquées/script.js
+++ 3.Les-fonctions-dans-le-détail/6.Fonctions-auto-invoquées/script.js
@@ -7,5 +7,13 @@
 
 // Syntaxe de base 
 
+(function () {
+    console.log("Hello world");
+})()
 
 // Avant l'arrivée de let & const qui règlent les problèmes de portée, on pouvait déclarer des var dans des IIFEE pour éviter de polluer la portée globale.
+
+var banana = "Test" // peut être atteint depuis window
+let apple = "Apple" // Ne  peut pas être atteint depuis window
+
+console.log(window);
