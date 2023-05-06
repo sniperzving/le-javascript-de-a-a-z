e0e931790c796c6b89853638a262321f5b5631a7
Ghiles Crp <ghiles.corp@gmail.com>
2023-05-06 14:54:40 +0200

    Modif de 4.Les-objets-dans-le-détail 2.Copier-un-objet

--- 4.Les-objets-dans-le-détail/2.Copier-un-objet/script.js
+++ 4.Les-objets-dans-le-détail/2.Copier-un-objet/script.js
@@ -6,9 +6,15 @@
 
 */
 
+const numbers = [1, 2, 3];
+const numbersCopy = numbers;
 
+console.log(numbersCopy);
 
+numbers.push(4);
 
+console.log(numbers);
+console.log(numbersCopy);
 
 /* 
     2. Alors comment copier ?
@@ -20,12 +26,29 @@
     Vous sentez le bug arriver ?
 */
 
+const letters = ["a", "b", "c", "d", { name: "Zoro" }];
 
+// Spread Operator
+const lettersCopy = [...letters];
+lettersCopy[4] = { ...letters[4] };
 
+/* lettersCopy[0] = "a";
+lettersCopy[1] = "b";
+lettersCopy[2] = "c";
+lettersCopy[3] = "d"; */
+// const lettersCopy = Array.from(letters);
+console.log(lettersCopy);
+letters.push("e");
 
+console.log(letters);
+console.log(lettersCopy);
 
+letters[4].name = "max";
 
+console.log(letters[4]);
+console.log(lettersCopy[4]);
 
+const shallowClone = structuredClone(letters);
 
 /* 
     3. Faire une copie profonde, deep copy.
@@ -37,3 +60,33 @@
     Malheuresement, cette méthode ne permet pas de cloner des méthodes.
 */
 
+const deepObj = {
+  userName: "Enzo",
+  age: 99,
+  collection: {
+    a: 1,
+    b: 2,
+    c: 3,
+  },
+};
+
+
+const objCopy = {
+    ...deepObj
+}
+
+deepObj.userName = "Julia";
+
+console.log(objCopy);
+
+deepCopy = {
+    collection: {
+        ...deepObj.collection
+    }
+}
+
+console.log(deepCopy);
+deepCopy.age = 11;
+
+console.log(deepObj);
+console.log(deepCopy);
