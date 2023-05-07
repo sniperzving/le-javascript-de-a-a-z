553848b3741c651e5652bd2dbbeeea264ad74036
Ghiles Crp <ghiles.corp@gmail.com>
2023-05-07 21:26:31 +0200

    Ajout de 7.Les-bases-du-JavaScript -7.Exercice2-Chaînes-fonctions-opérateurs

---
+++ 7.Les-bases-du-JavaScript/7.Exercice2-Chaînes-fonctions-opérateurs/script.js
@@ -0,0 +1,33 @@
+// EXERCIE2 : Les opérateurs, chaînes & fonctions
+
+/*  Ecrivez une fonction qui retourne un nombre multiplié 
+par deux en utilisant un caramètre
+ */
+
+
+function multiplie(nb) {
+    return nb*2;
+}
+
+console.log(multiplie(2));
+
+
+// Déterminez ce qui est vrai ou faux
+// let solde = 150 000; FAUX
+// solde += 50 0000 FAUX
+
+
+// Ce code permet d'assigner la valeur 50 000 à solde
+// Faux
+
+let resultat = 10%7;
+console.log(resultat);
+
+// result est égal à 5
+// FAUX
+
+let str = `J'ai besoin de $(nbPneus) pour ma voiture`;
+console.log(str);
+
+// C'est la bonne façon d'intégrer une expression dans une chaîne
+// FAUX
