e8370e63bdb78be28fc3b03e103a2465338a95de
Ghiles Crp <ghiles.corp@gmail.com>
2023-05-07 21:37:31 +0200

    Ajout de 7.Les-bases-du-JavaScript -9.Introduction-aux-tableaux

---
+++ 7.Les-bases-du-JavaScript/10.Introduction-aux-objets/script.js
@@ -0,0 +1,11 @@
+let tableau = [1,2, 3, 4, 5];
+
+console.log(tableau);
+console.log(tableau[0]);
+console.log(tableau[tableau.length-1]);
+
+
+tableau.push(6);
+console.log(tableau);
+tableau.pop();
+console.log(tableau);
