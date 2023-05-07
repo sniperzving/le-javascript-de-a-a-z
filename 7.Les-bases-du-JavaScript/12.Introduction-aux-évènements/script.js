d304e721c1ed23a8ac263c9720151b4c13f4cee4
Ghiles Crp <ghiles.corp@gmail.com>
2023-05-07 22:06:01 +0200

    Ajout de 7.Les-bases-du-JavaScript -12.Introduction-aux-évènements

---
+++ 7.Les-bases-du-JavaScript/12.Introduction-aux-évènements/script.js
@@ -0,0 +1,11 @@
+const titre = document.querySelector("h1");
+
+
+
+titre.addEventListener('click', fonctionAnimation);
+titre.addEventListener('mouseenter', fonctionAnimation);
+
+
+function fonctionAnimation() {
+    console.log("Clicked");  
+  }
