9347340f986d8dce7f12826750d056854540055b
Ghiles Crp <ghiles.corp@gmail.com>
2023-05-07 17:23:06 +0200

    Modif de 5.Les-tableaux-dans-le-détail 7-Itérer-un-tableau-multidimensionnel

--- 5.Les-tableaux-dans-le-détail/7-Itérer-un-tableau-multidimensionnel/script.js
+++ 5.Les-tableaux-dans-le-détail/7-Itérer-un-tableau-multidimensionnel/script.js
@@ -9,5 +9,22 @@
     On peut itérer à travers ce genre de tableau avec une double boucle for.
 */
 
-const couples = [["Tom","Lea"], ["Jack", "Sara"], ["Pedro", "Maria"]];
+const couples = [
+  ["Tom", "Lea"],
+  ["Jack", "Sara"],
+  ["Pedro", "Maria"],
+];
 
+// Boucle for normale
+for (let i = 0; i < couples.length; i++) {
+  for (let j = 0; j < couples[i].length; j++) {
+    console.log(couples[i][j]);
+  }
+}
+
+// Boucle for...of
+for (const iterator1 of couples) {
+  for (const iterator2 of iterator1) {
+    console.log(iterator2);
+  }
+}
