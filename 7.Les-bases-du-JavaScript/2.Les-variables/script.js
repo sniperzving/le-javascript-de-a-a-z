2cd42689ea765c0348b7571f226c7232fe302d73
sniperzving <114675858+sniperzving@users.noreply.github.com>
2023-08-14 13:08:20 +0200

    First commit

--- 7.Les-bases-du-JavaScript/2.Les-variables/script.js
+++
@@ -1,8 +0,0 @@
-let prenom = "Ghiles";
-
-prenom = "Katty";
-console.log(prenom);
-
-
-const taille = 185;
-console.log(taille);
--- 7.Les-bases-du-JavaScript/4.Les-opérateurs/script.js
+++
@@ -1,4 +0,0 @@
-// Les opérateurs
-let nb1 = 0;
-nb1 = 2**3;
-console.log(nb1);
